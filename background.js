71a88d0c09079091f7a638ea36bd0f4e2547662b
gexing <imaxing@126.com>
2021-01-11 15:07:17 +0800

    update

--- background.js
+++ background.js
@@ -7,54 +7,69 @@
 const targetPage = 'zhipin.com' // 目标站点
 const mainSiteURL = 'https://www.wondercv.com/cvs' // 主站url
 const mainSiteDomain = '.wondercv.com' // 主站的domain
-const cookieName = 'sdktoken' // 判断登录状态的cookie的name
-
-// installed
-chrome.runtime.onInstalled.addListener(function() {
-  // 插件安装的时候判断登录状态
-  checkisAuthed()
-})
-
-// 点击图标的时候执行的逻辑
-chrome.browserAction.onClicked.addListener(function(tab) {
-  const auth = checkisAuthed()
-  if (auth) {
-    chrome.tabs.update(tab.id, { selected: true, url: mainSiteURL })
-  } else {
-    alert('未登录')
-  }
-})
+const cookieName = 'grwng_uid1' // 判断登录状态的cookie的name
 
 
-chrome.windows.onCreated.addListener(function(createInfo) {
-  // 打开window的时候判断登录状态
-  checkisAuthed()
-})
 
 // 动态设置browser_action的icon
 function setBrowserActionIcon(iconPath) {
   chrome.browserAction.setIcon({ path: iconPath })
 }
 
+// 判断登录状态
+function checkIsAuthored() {
+  return new Promise((resolve, reject) => {
+    chrome.cookies.get({ name: cookieName, url: 'https://www.wondercv.com'  }, function(cookie) {
+      if (cookie) {
+        setBrowserActionIcon("images/get_started16.png")
+      } else {
+        setBrowserActionIcon("images/get_started_unlogin_16.png")
+      }
+      resolve(cookie)
+    })
+  })
+}
+
+
+// 插件安装的时候判断登录状态
+chrome.runtime.onInstalled.addListener(() => checkIsAuthored())
+
+
+// 打开window的时候判断登录状态
+chrome.windows.onCreated.addListener(() => checkIsAuthored())
+
+
+// 点击图标的时候执行的逻辑
+chrome.browserAction.onClicked.addListener(async function(tab) {
+  const auth = await checkIsAuthored()
+  if (auth) {
+    chrome.tabs.update(tab.id, { selected: true, url: mainSiteURL })
+  } else {
+    chrome.browserAction.setPopup({ popup: 'popup/popup.html' })
+  }
+})
+
+
 // 在主站执行登录退出时候判断登录状态
 chrome.cookies.onChanged.addListener(function(changeInfo) {
   const { cookie } = changeInfo || {}
   if (cookie && cookie.domain && cookie.domain.indexOf(mainSiteDomain) !== 1) {
-    checkisAuthed()
+    checkIsAuthored()
   }
 })
 
-// 判断登录状态
-function checkisAuthed() {
-  chrome.cookies.get({ name: cookieName, url: 'http://www.wondercv.com/talent'  }, function(cookie) {
-    if (cookie) {
-      setBrowserActionIcon("images/get_started16.png")
-      return true
+
+// 用户访问目标站点的时候执行的逻辑
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.active && tab.selected && tab.status === 'complete') {
+    if (tab.url.indexOf(targetPage) !== -1) {
+      sendMsgToContentScript({ type: 'get_info', value: 'ssss' })
     }
-    setBrowserActionIcon("images/get_started_unlogin_16.png")
-    return false
-  })
-}
+  }
+})
+
+
+
 
 // 向 content 发消息
 function sendMsgToContentScript(message, callback) {
@@ -71,13 +86,4 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log('收到来自content的消息：' + request.value)
   }
   sendResponse('我是后台，已收到你的消息。')
-})
-
-// 用户访问目标站点的时候执行的逻辑
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (tab.active && tab.selected && tab.status === "complete") {
-    if (tab.url.indexOf(targetPage) !== -1) {
-      sendMsgToContentScript({ type: 'get_info', value: 'ssss' })
-    }
-  }
 })
--- content/script.js
+++ content/script.js
@@ -1,54 +1,73 @@
 'use strict'
 
-// function Recommend(data) {
-//   this.data = data || {}
-// }
 
-// Recommend.prototype.render = function (data) {
 
-//   this.close()
-//   const el = document.createElement('div')
-  
-//   el.innerHTML = `
-//     <h2>超级简历职位分析</h2>
-//     <span class="close_el">关闭</span>
-//     ${JSON.stringify(data)}
-//   `
-//   el.className = 'job_info'
-//   document.body.appendChild(el)
-//   const timer = setTimeout(() => {
-//     const close_el = document.querySelector('.close_el')
-//     if (close_el) {
-//       close_el.addEventListener('click', this.close, false)
-//     }
-//     clearTimeout(timer)
-//   }, 0)
-// }
+const prefixClass = 'job-scan'
+const modalID = `${prefixClass}-modal`
 
-// Recommend.prototype.close = function () {
-//   const job_info = document.querySelector('.job_info')
-//   document.body.removeChild(job_info)
-// }
 
-// Recommend.prototype.getUserState = function () {
-//   const cookies = document.cookies
-//   this.render({ cookies })
-// }
 
-// const recommendIns = new Recommend()
+
+
+// 检测简历 dom class 是否出现在页面中
+function getCvResumeInfo() {
+  const cvDom = document.querySelector('.job-sec')
+  if(cvDom) {
+    alert('检测到岗位描述信息, 弹出弹窗')
+  }
+  return cvDom.innerHTML || null
+}
+
+// 关闭窗口
+function closeModal () {
+  let modalEl = document.querySelector(`#${modalID}`)
+  if (modalEl) {
+    document.body.removeChild(modalEl)
+  }
+}
 
 
 // 接收插件后台消息
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if(request.type === 'get_info') {
-    let cvInfo = getCvInfo()
-    if (cvInfo) {
-      renderModal(cvInfo)
+    let resumeInfo = getCvResumeInfo()
+    if (resumeInfo) {
+      renderModal(resumeInfo)
+    } else {
+      closeModal()
     }
   }
-
   sendResponse('我是content,已收到你的消息')
 })
+
+
+function modalTemplate(resumeInfo) {
+  return `
+    <div class="${prefixClass}-header">
+      <h2>超级简历职位分析</h2>
+      <span id="${prefixClass}-close-btn" class="close-btn">关闭</span>
+    </div>
+    <div class="${prefixClass}-body">${resumeInfo}</div>
+  `
+}
+
+function renderModal(cvInfo) {
+  let modalEl = document.querySelector(`#${modalID}`)
+  const modalContent = modalTemplate(cvInfo)
+
+  if (!modalEl) {
+    modalEl = document.createElement('div')
+    modalEl.innerHTML = modalContent
+    modalEl.id = modalID
+    modalEl.className = `${prefixClass}-box`
+    document.body.appendChild(modalEl)
+    
+  } else {
+    modalEl.innerHTML = modalContent
+  }
+  document.querySelector(`#${prefixClass}-close-btn`).addEventListener('click', closeModal)
+}
+
 
 
 // // 向插件后台发消息
@@ -57,46 +76,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 //     callback && callback(response)
 //   })
 // }
-
-const prefixClass = 'job-scan'
-const modalID = `${prefixClass}-modal`
-const targetCvClass = '.job-list ul'
-
-function getCvInfo() {
-  const cVdom = document.querySelector(targetCvClass)
-  if (cVdom) {
-    return cVdom.innerHTML
-  }
-  return ''
-}
-
-function renderModal(cvInfo) {
-  let modalEl = document.querySelector(`#${modalID}`)
-  
-  if (!modalEl) {
-    modalEl = document.createElement('div')
-  
-    modalEl.innerHTML = `
-      <div class="${prefixClass}-header">
-        <h2>超级简历职位分析</h2>
-        <span id="${prefixClass}-close-btn" class="close-btn">关闭</span>
-      </div>
-      <div class="${prefixClass}-body">${cvInfo}</div>
-    `
-    modalEl.id = modalID
-    modalEl.className = `${prefixClass}-box`
-    document.body.appendChild(modalEl)
-  } else {
-    modalEl.innerHTML = `
-      <div class="${prefixClass}-header">
-        <h2>超级简历职位分析</h2>
-        <span id="${prefixClass}-close-btn" class="close-btn">关闭</span>
-      </div>
-      <div class="${prefixClass}-body">${cvInfo}</div>
-    `
-  }
-
-  document.querySelector(`#${prefixClass}-close-btn`).addEventListener('click', function() {
-    document.body.removeChild(modalEl)
-  })
-}
